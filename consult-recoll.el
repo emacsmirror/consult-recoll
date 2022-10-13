1480027006df774ac3e2f38a6750a369263ea409
jao <jao@gnu.org>
2022-10-13 23:33:19 +0100

    changelog and version bump

--- consult-recoll.el
+++ consult-recoll.el
@@ -4,8 +4,8 @@
 ;; Maintainer: Jose A Ortega Ruiz <jao@gnu.org>
 ;; Keywords: docs, convenience
 ;; License: GPL-3.0-or-later
-;; Version: 0.7
-;; Package-Requires: ((emacs "26.1") (consult "0.18"))
+;; Version: 0.8
+;; Package-Requires: ((emacs "26.1") (consult "0.19"))
 ;; Homepage: https://codeberg.org/jao/consult-recoll
 
 ;; Copyright (C) 2021-2022  Free Software Foundation, Inc.
